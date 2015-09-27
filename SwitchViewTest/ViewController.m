94baeacfcec95df38b64f77b141c02688c1493e6
郭嘉 <guojia@guojiadeMacBook-Pro.local>
2015-09-27 14:56:40 +0800

    12

--- SwitchViewTest/ViewController.m
+++ SwitchViewTest/ViewController.m
@@ -17,6 +17,17 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    UISwitch *defaultSwitch = [[UISwitch alloc] init];
+    [defaultSwitch addTarget:self action:@selector(switchCornorRadius:) forControlEvents:UIControlEventValueChanged];
+//    defaultSwitch.center = self.view.center;
+    defaultSwitch.frame = CGRectMake(300, 100, 300, 80);
+    defaultSwitch.on = YES;
+//    defaultSwitch.thumbTintColor=[UIColor purpleColor];
+//    defaultSwitch.onTintColor = [UIColor redColor];
+//    defaultSwitch.tintColor = [UIColor redColor];
+//    [defaultSwitch setOn:NO animated:YES];
+    [self.view addSubview:defaultSwitch];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -24,4 +35,13 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (void)switchCornorRadius:(UISwitch *)sender{
+    if (sender.on) {
+        UIAlertView *alert=[[UIAlertView alloc]initWithTitle:@"提示" message:@"打开" delegate:self cancelButtonTitle:@"确定" otherButtonTitles:nil];
+        [alert show];
+    }else{
+        UIAlertView *alert=[[UIAlertView alloc]initWithTitle:@"提示" message:@"关闭" delegate:self cancelButtonTitle:@"确定" otherButtonTitles:nil];
+        [alert show];
+    }
+}
 @end
